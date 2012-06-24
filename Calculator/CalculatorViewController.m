6f410bfd9197fe0b161164ef0dd3e7b52cd86a4c
Jason Kirchner <jason.kirchner@me.com>
2012-06-24 12:34:13 -0600

    Extra Credit Implemented. Final Homework 1

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -16,6 +16,7 @@
 
 @implementation CalculatorViewController
 @synthesize display = _display;
+@synthesize history = _history;
 @synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
 @synthesize brain = _brain;
 
@@ -29,7 +30,33 @@
 {
     self.brain = nil;
     self.display.text = @"0";
+    self.history.text = @"";
     self.userIsInTheMiddleOfEnteringANumber = NO;
+}
+
+- (void)printHistory:(NSString *)newOperation
+{
+    self.history.text = [self.history.text stringByAppendingFormat:@" %@", newOperation];
+}
+
+- (IBAction)backSpacePressed 
+{
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        if (self.display.text.length == 1) {
+            self.display.text = @"0";
+            self.userIsInTheMiddleOfEnteringANumber = NO;
+        }
+        else                
+            self.display.text = [self.display.text substringToIndex:(self.display.text.length - 1)];
+    } else 
+        self.display.text = @"0";
+}
+
+- (IBAction)chanageSign 
+{
+    if ([self.display.text doubleValue]) {
+        self.display.text = [NSString stringWithFormat:@"%g", ([self.display.text doubleValue]) * -1];
+    }
 }
 
 - (IBAction)digitPressed:(UIButton *)sender 
@@ -49,7 +76,7 @@
         
     }
     if (self.userIsInTheMiddleOfEnteringANumber) {
-        self.display.text = [self.display.text stringByAppendingString:[sender currentTitle]];
+        self.display.text = [self.display.text stringByAppendingString:sender.currentTitle];
     } else {
         self.display.text = sender.currentTitle;
         self.userIsInTheMiddleOfEnteringANumber = YES;
@@ -61,14 +88,20 @@
     if (self.userIsInTheMiddleOfEnteringANumber)
         [self enterPressed];
     
-    double result = [self.brain performOperation:[sender currentTitle]];
+    double result = [self.brain performOperation:sender.currentTitle];
+    [self printHistory:sender.currentTitle];
     self.display.text = [NSString stringWithFormat:@"%g", result];
 }
 
 - (IBAction)enterPressed 
 {
-    [self.brain pushOperand:[self.display.text doubleValue]];
+    [self.brain pushOperand:self.display.text.doubleValue];
     self.userIsInTheMiddleOfEnteringANumber = NO;
+    [self printHistory:self.display.text];
 }
 
+- (void)viewDidUnload {
+    [self setHistory:nil];
+    [super viewDidUnload];
+}
 @end
