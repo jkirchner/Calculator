ce559710029b1376614a1b20bea8c662fd378567
Jason Kirchner <jason.kirchner@me.com>
2012-06-25 20:07:48 -0600

    implemented class methods popOpperandOffProgramStack and runProgram.

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -36,47 +36,58 @@
     [self.operandStack addObject:operandObject];
 }
 
-- (double)popOperand
-{
-    NSNumber *operandObject = [self.operandStack lastObject];
-    if (operandObject) [self.operandStack removeLastObject];
-    return operandObject.doubleValue;
-}
-
 - (double)performOperation:(NSString *)operation
 {
     double result = 0;
     
-    if ([operation isEqualToString:@"+"]) {
-        result = [self popOperand] + [self popOperand];
-    } else if ([operation isEqualToString:@"-"]) {
-        double subtractor = [self popOperand];
-        result = [self popOperand] - subtractor;
-    } else if ([operation isEqualToString:@"*"]) {
-        result = [self popOperand] * [self popOperand];
-    } else if ([operation isEqualToString:@"/"]) {
-        double divisor = [self popOperand];
-        if (divisor) result = [self popOperand] / divisor;
-    } else if ([operation isEqualToString:@"Sin"]) {
-        result = sin([self popOperand]);
-    } else if ([operation isEqualToString:@"Cos"]) {
-        result = cos([self popOperand]);
-    } else if ([operation isEqualToString:@"√"]) {
-        double operand = [self popOperand];
-        if (operand > 0) result = sqrt(operand);
-    } else if ([operation isEqualToString:@"π"]) {
-        result = M_PI;
-    }
     [self pushOperand:result];
     
     return result;
 }
 
-+ (double)runProgram:(id)program
++ (double)popOperandOffProgramStack:(NSMutableArray *)stack
 {
     double result = 0;
     
+    id topOfStack = [stack lastObject];
+    if (topOfStack) [stack removeLastObject];
+
+    if ([topOfStack isKindOfClass:[NSNumber class]])
+    {
+        result = [topOfStack doubleValue];
+    } else if ([topOfStack isKindOfClass:[NSString class]]){
+        NSString *operation = topOfStack;
+        if ([operation isEqualToString:@"+"]) {
+            result = [self popOperandOffProgramStack:stack] + [self popOperandOffProgramStack:stack];
+        } else if ([operation isEqualToString:@"-"]) {
+            double subtractor = [self popOperandOffProgramStack:stack];
+            result = [self popOperandOffProgramStack:stack] - subtractor;
+        } else if ([operation isEqualToString:@"*"]) {
+            result = [self popOperandOffProgramStack:stack] * [self popOperandOffProgramStack:stack];
+        } else if ([operation isEqualToString:@"/"]) {
+            double divisor = [self popOperandOffProgramStack:stack];
+            if (divisor) result = [self popOperandOffProgramStack:stack] / divisor;
+        } else if ([operation isEqualToString:@"Sin"]) {
+            result = sin([self popOperandOffProgramStack:stack]);
+        } else if ([operation isEqualToString:@"Cos"]) {
+            result = cos([self popOperandOffProgramStack:stack]);
+        } else if ([operation isEqualToString:@"√"]) {
+            double operand = [self popOperandOffProgramStack:stack];
+            if (operand > 0) result = sqrt(operand);
+        } else if ([operation isEqualToString:@"π"]) {
+            result = M_PI;
+        }
+    }
+
     return result;
+}
+
++ (double)runProgram:(id)program
+{
+    NSMutableArray *stack;
+    if ([program isKindOfClass:[NSArray class]]) 
+        stack = [program mutableCopy];
+    return [self popOperandOffProgramStack:stack];
 }
 
 + (double)runProgram:(id)program usingVariables:(NSDictionary *)variableValues
@@ -100,6 +111,23 @@
 
 + (BOOL)isOperation:(NSString *)operation
 {
-    
+    return NO;
+}
+
++ (NSSet *)operations;
+{
+    NSSet *allOperations = [NSSet setWithObjects:@"π", nil];
+    allOperations = [allOperations setByAddingObjectsFromSet:[self twoOperandOperations]];
+    return [allOperations setByAddingObjectsFromSet:[self oneOperandOperations]];
+}
+
++ (NSSet *)twoOperandOperations
+{
+    return [NSSet setWithObjects:@"+", @"-", @"*", @"/", nil];
+}
+
++ (NSSet *)oneOperandOperations
+{
+    return [NSSet setWithObjects:@"√", @"Sin", @"Cos", nil];
 }
 @end
